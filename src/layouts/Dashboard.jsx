2d4d546bb693a5499d92f17af92e30e0329653d2
muhittinayri <ayri.muhittin@gmail.com>
2022-04-17 00:59:35 +0300

    17 April 2022

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import ProductList from '../pages/ProductList'
-import Categories from './Categories'
-
+import React from "react";
+import ProductList from "../pages/ProductList";
+import Categories from "./Categories";
+import { Grid } from "semantic-ui-react";
 
 export default function Dashboard() {
   return (
-    <div>    
-      <Categories></Categories>
-      <ProductList></ProductList>
+    <div>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={4}>
+            <Categories></Categories>
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <ProductList></ProductList>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
-  )
+  );
 }
